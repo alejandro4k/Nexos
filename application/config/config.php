71bea502aaadec8186a282a7e0eb15accd516f69
Jhoan Alejandro Anaya <jaanaya88@misena.edu.co>
2020-03-01 22:24:43 -0500

    chage base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Api';
+$config['base_url'] = 'http://thawing-ocean-79982.herokuapp.com/Api';
 
 /*
 |--------------------------------------------------------------------------
