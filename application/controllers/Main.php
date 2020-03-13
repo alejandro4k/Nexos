17ab1f939d6f0fed4a7b382658c47273ae2dd4dc
Jhoan Alejandro Anaya <jaanaya88@misena.edu.co>
2020-03-12 19:28:19 -0500

    config send email 2

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -75,7 +75,7 @@ class Main extends CI_Controller{
                 $this->load->library('email');
                 $config['protocol']='smtp';
                 $config['smtp_host']='https://apinexos.herokuapp.com/';
-                $config['smtp_port']='465';
+                $config['smtp_port']='587';
                 $config['smtp_timeout']='30';
                 $config['smtp_user']='your mail id';
                 $config['smtp_pass']='your password';
