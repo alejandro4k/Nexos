abb29a72281e8c678d3339e3d2999fe743e8ec35
Jhoan Alejandro Anaya <jaanaya88@misena.edu.co>
2020-03-12 19:51:06 -0500

    config send email 6

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -75,7 +75,7 @@ class Main extends CI_Controller{
                 $this->load->library('email');
                 $config['protocol'] = 'smtp';
                 $config['smtp_host'] = 'smtp.gmail.com';
-                $config['smtp_port'] = '465';
+                $config['smtp_port'] = '587';
                 $config['_smtp_auth']=TRUE;
                 $config['smtp_user'] = 'jhoanalejandro.anaya@gmail.com';
                 $config['smtp_pass'] = 'jhoanalejandro1';
@@ -84,6 +84,7 @@ class Main extends CI_Controller{
                 $config['wordwrap'] = TRUE;
                 $config['mailtype'] = "html";
                 $this->email->initialize($config);
+                $this->email->set_newline("\r\n");
                 $this->email->from('jhoanalejandro.anaya@gmail.com','Alejandro Anaya');
                 $this->email->to($Email);
                 $this->email->subject("Bienvenido");
