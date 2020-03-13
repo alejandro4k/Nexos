b61d8e3b71d70fc0553320f865e8d8bc07c0912e
Jhoan Alejandro Anaya <jaanaya88@misena.edu.co>
2020-03-12 20:30:32 -0500

    config smtp 2

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -73,15 +73,18 @@ class Main extends CI_Controller{
             if($response){
                 
                 $this->load->library('email');
-                $config['protocol'] = 'smtp';
-                $config['smtp_host'] = 'smtp.googlemail.com';
-                $config['smtp_port'] = 587;
-                $config['smtp_user'] = 'nexostest1@gmail.com';
-                $config['smtp_pass'] = 'nexos1234';
-                $config['smtp_timeout']='30';
-                $config['mailpath'] = '/usr/sbin/sendmail';
-                $config['charset'] = 'utf-8';             
-                $config['mailtype'] = "html";
+                $config = Array(
+                    'protocol' => 'smtp',
+                    'smtp_host' => 'ssl://smtp.googlemail.com',
+                    'smtp_port' => 465,
+                    'smtp_user' => 'nexostest1@gmail.com',
+                    'smtp_pass' => 'nexos1234',
+                    'mailtype'  => 'html', 
+                    'charset' => 'utf-8',
+                    'wordwrap' => TRUE
+                );
+
+                
                 $this->email->initialize($config);
                 $this->email->set_newline("\r\n");
                 $this->email->from('nexostest1@gmail.com');
